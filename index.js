39b5b545480b8ca02188116f3dd980d9c8ebc8b3
Chris Wehner <chrisw7128@gmail.com>
2023-01-06 15:09:52 -0800

    profile upload

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
+let content = document.querySelector(".js-generated.content");
 
-let content = document.querySelector('.js-generated.content')
+let header = document.createElement("h1");
+header.setAttribute("class", "dog-name");
+header.append("Rizzo");
+content.append(header);
 
-let header = document.createElement('h1')
-header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
-content.append(header)
+let dogContent = document.createElement("div");
+dogContent.setAttribute("class", "dog-content");
+content.append(dogContent);
 
-let dogContent = document.createElement('div')
-dogContent.setAttribute('class', 'dog-content')
-content.append(dogContent)
+let dogImage = document.createElement("img");
+dogImage.setAttribute("class", "dog-image");
+dogImage.setAttribute("src", "./assets/rizzo.jpg");
+dogContent.append(dogImage);
 
-let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
-dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+let dogDetails = document.createElement("div");
+dogDetails.setAttribute("class", "dog-details");
+dogContent.append(dogDetails);
 
-let dogDetails = document.createElement('div')
-dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+let header3 = document.createElement("h3");
+header3.textContent = "Description:";
+dogDetails.append(header3);
 
+let p1 = document.createElement("p1");
+p1.textContent =
+  "This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.";
+dogDetails.append(p1);
 
+let feeding = document.createElement("h3");
+feeding.textContent = "Feeding Times:";
+dogDetails.append(feeding);
+
+let list = document.createElement("ul");
+
+dogDetails.append(list);
+
+let morning = document.createElement("li");
+let afternoon = document.createElement("li");
+let evening = document.createElement("li");
+
+morning.textContent = "9:00 am";
+afternoon.textContent = "12:00 pm";
+evening.textContent = "5:00 pm";
+
+list.append(morning);
+list.append(afternoon);
+list.append(evening);
